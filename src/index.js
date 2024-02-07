0493fc614cbc1ce9696885719cf50859898ab85f
Full-stack developer <lovetom.pro@gmail.com>
2024-02-07 07:08:56 -0500

    env

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const todoRouter = require("./routes/todos");
-
 const app = express();
+require("dotenv").config();
 const PORT = process.env.PORT || 4000;
 
 // app configs.
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
